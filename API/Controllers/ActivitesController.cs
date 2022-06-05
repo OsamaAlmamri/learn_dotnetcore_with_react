5d6c94638f7915e6b5440d83fb0ca515c2199930
OsamaAlmamri <yemencoder@gmail.com>
2022-06-05 21:54:04 +0300

    show details

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -26,11 +26,13 @@ namespace API.Controllers
 
               [HttpGet("{id}")]
 
-         public async Task<ActionResult<Activity>> GetSingleActivites(Guid id)
+           public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
 
+
+       
 
 
     }
