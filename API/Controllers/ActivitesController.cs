647fe81a020a19ac02cbde56c358f627d4dea5a6
OsamaAlmamri <yemencoder@gmail.com>
2022-06-05 22:37:58 +0300

    delete

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -47,6 +47,12 @@ namespace API.Controllers
         }
 
 
-       
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
+        }
+
+
     }
 }
